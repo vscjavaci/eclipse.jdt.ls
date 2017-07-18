9b65f148103fa6c1691553720dd943734cbed02a
Rome Li <roml@microsoft.com>
2017-07-18 18:54:20 +0800

    Replace tabs with spaces

--- org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/internal/DebugEvent.java
+++ org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/internal/DebugEvent.java
@@ -3,6 +3,6 @@ package org.eclipse.jdt.ls.debug.internal;
 import com.sun.jdi.event.Event;
 
 public class DebugEvent {
-	public Event event = null;
-	public boolean shouldResume = true;
+    public Event event = null;
+    public boolean shouldResume = true;
 }
--- org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/internal/EventHub.java
+++ org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/internal/EventHub.java
@@ -10,47 +10,48 @@ import io.reactivex.Observable;
 import io.reactivex.subjects.PublishSubject;
 
 public class EventHub implements AutoCloseable {
-	private PublishSubject<DebugEvent> subject = PublishSubject.<DebugEvent>create();
-	
-	public Observable<DebugEvent> observable() {
-		return subject;
-	}
-	
-	/**
-	 * @param vm - the target virtual machine.
-	 */
-	public void start(VirtualMachine vm) {
-		EventQueue queue = vm.eventQueue();
-		while (true) {
-			try {
-				if (Thread.interrupted()) {
-					break;
-				}
-				
-				EventSet set = queue.remove();
-				boolean shouldResume = true;
-				for (Event event : set) {
-					DebugEvent dbgEvent = new DebugEvent();
-					dbgEvent.event = event;
-					subject.onNext(dbgEvent);
-					shouldResume &= dbgEvent.shouldResume;
-				}
-				
-				if (shouldResume) {
-					set.resume();
-				}
-			} catch (InterruptedException e) {
-				subject.onComplete();
-				return;
-			} catch (VMDisconnectedException e) {
-				subject.onError(e);
-				return;
-			}
-		}
-	}
-	
-	@Override
-	public void close() {
-		Thread.currentThread().interrupt();
-	}
+    private PublishSubject<DebugEvent> subject = PublishSubject.<DebugEvent>create();
+
+    public Observable<DebugEvent> observable() {
+        return subject;
+    }
+
+    /**
+     * @param vm
+     *            - the target virtual machine.
+     */
+    public void start(VirtualMachine vm) {
+        EventQueue queue = vm.eventQueue();
+        while (true) {
+            try {
+                if (Thread.interrupted()) {
+                    break;
+                }
+
+                EventSet set = queue.remove();
+                boolean shouldResume = true;
+                for (Event event : set) {
+                    DebugEvent dbgEvent = new DebugEvent();
+                    dbgEvent.event = event;
+                    subject.onNext(dbgEvent);
+                    shouldResume &= dbgEvent.shouldResume;
+                }
+
+                if (shouldResume) {
+                    set.resume();
+                }
+            } catch (InterruptedException e) {
+                subject.onComplete();
+                return;
+            } catch (VMDisconnectedException e) {
+                subject.onError(e);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void close() {
+        Thread.currentThread().interrupt();
+    }
 }
