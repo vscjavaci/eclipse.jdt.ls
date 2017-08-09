ae9a50731c6e719a76838cb99421710d61db6438
andxu <andxu@microsoft.com>
2017-08-09 11:08:42 +0800

    add map import
    Signed-off-by: andxu <andxu@microsoft.com>

--- org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/adapter/BreakpointManager.java
+++ org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/adapter/BreakpointManager.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.eclipse.jdt.ls.debug.IBreakpoint;
