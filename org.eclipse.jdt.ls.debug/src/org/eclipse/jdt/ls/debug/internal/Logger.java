4303f06c3ae5d8a0e7db8aef0b2822c085fdc5ab
andxu <andxu@microsoft.com>
2017-08-15 15:23:51 +0800

    remove local debug code
    Signed-off-by: andxu <andxu@microsoft.com>

--- org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/internal/Logger.java
+++ org.eclipse.jdt.ls.debug/src/org/eclipse/jdt/ls/debug/internal/Logger.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
-* Copyright (c) 2017 Microsoft Corporation and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*     Microsoft Corporation - initial API and implementation
-*******************************************************************************/
+ * Copyright (c) 2017 Microsoft Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Microsoft Corporation - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.jdt.ls.debug.internal;
 
@@ -25,17 +25,14 @@ public class Logger {
         if (isVerbose) {
             JavaDebuggerServerPlugin.logInfo(message);
         }
-        System.out.println(message);
     }
 
     public static void logException(String message, Exception e) {
         JavaDebuggerServerPlugin.logException(message, e);
-        e.printStackTrace();
     }
 
     public static void logError(String error) {
         JavaDebuggerServerPlugin.logError(error);
-        System.err.println(error);
     }
 
 }
